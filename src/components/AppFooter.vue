9c5bce431a626a0e502b705c7a472076eedef1c9
Luca Cassano <lucacassano96@gmail.com>
2023-05-31 22:31:45 +0200

    completato header e footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,6 @@ export default {
 </script>
 
 <template>
-    
     <footer>
 
         <section class="info">
@@ -27,58 +26,214 @@ export default {
                 <img src="src/assets/img/buy-comics-subscriptions.png" alt="">
                 <span>COMIC SHOP LOCATOR</span>
             </div>
-    
+
             <div class="card">
                 <img src="src/assets/img/buy-dc-power-visa.svg" alt="">
                 <span>DC PWER VISA</span>
             </div>
 
+        </section>
+
+        <section id="dc">
+            <div class="links">
+                <div>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li><a href="#">Character</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Comics</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Movies</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">TV</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Games</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <h3>SHOP</h3>
+
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC COllectibles</a></li>
+
+                    </ul>
+
+                </div>
+
+                <div>
+
+                    <h3>DC </h3>
+                    <ul>
+                        <li><a href="#">Character</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Comics</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Movies</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">TV</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Games</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </div>
+
+
+                <div>
+
+                    <h3> SITES </h3>
+                    <ul>
+                        <li><a href="#">Character</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Comics</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="#">Movies</a></li>
+                    </ul>
+
+                </div>
+            </div>
+
+            <div class="logo">
+                <img src="src/assets/img/dc-logo-bg.png" alt="">
+            </div>
 
         </section>
 
-        <section>
+        <section id="contacts">
 
-        </section>
+            <div class="Sign">Sign Up Now!</div>
 
-        <section>
-            
+            <div class="follow"><a href="#">FOLLOW US</a></div>
+
+            <div class="socials">
+
+                
+                <div> <img src="src/assets/img/footer-facebook.png" alt=""></div>
+                <div> <img src="src/assets/img/footer-periscope.png" alt=""></div>
+                <div> <img src="src/assets/img/footer-pinterest.png" alt=""></div>
+                <div> <img src="src/assets/img/footer-twitter.png" alt=""></div>
+                <div> <img src="src/assets/img/footer-youtube.png" alt=""></div>
+
+            </div>
+
+
         </section>
 
 
     </footer>
-  
-
-
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/variables.scss" as *;
 
-.info{
+.info {
     width: 100%;
-    height: 120px;
+    height: 100px;
     background-color: $bluecolor;
     color: $whitecolor;
     display: flex;
     justify-content: center;
     align-content: center;
 
- .card{
+    .card {
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 0 20px;
-    font-size: 14px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 20px;
+        font-size: 14px;
 
-    img{
-        width: 50px;
-        padding: 7px;
+        img {
+            width: 50px;
+            padding: 7px;
+        }
     }
- }
-
-   
 
 }
 
+#dc {
+    width: 100%;
+    background-image: url("src/assets/img/footer-bg.jpg");
+    display: flex;
+    justify-content: space-between;
+
+
+    .links {
+        width: 40%;
+        display: flex;
+        position: relative;
+        color: $whitecolor;
+        justify-content: space-evenly;
+        padding: 20px;
+
+        h3 {
+            padding: 7px 0;
+        }
+
+        li {
+            padding: 5px 0px;
+        }
+
+        a {
+            color: $greycolor;
+            font-size: $fontsize;
+
+        }
+    }
+
+    .logo {
+        width: 40%;
+
+        img {
+            width: 400px;
+        }
+    }
+}
+
+#contacts {
+    width: 100%;
+    height: 100px;
+    background-color: $darkgrey;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    .Sign {
+        color: $whitecolor;
+        padding: 10px;
+        border: 3px solid $bluecolor;
+    }
+
+    .follow{
+        a {
+            color: $bluecolor;
+            font-size: 18px;
+        }
+    }
+
+    .socials {
+        display: flex;
+
+        img {
+            padding: 0 10px;
+        }
+
+       
+    }
+
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,59 @@
 <script>
 
 export default {
-    name: "AppHeader"
+    name: "AppHeader",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                },
+
+            ]
+        }
+    }
 }
 </script>
 
@@ -13,13 +65,10 @@ export default {
         </div>
 
         <ul>
-            <li>ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
-            <li>ciao</li>
+            <li v-for="link in links">
+                <a :href="link.url" :class="link.current ? 'active' : ''"> {{ link.text }}</a>
+            </li>
+
 
         </ul>
     </header>
@@ -30,21 +79,32 @@ export default {
 
 header {
     width: 100%;
-    height: 120px;
-    border: 1px solid #000;
+    height: 100px;
     display: flex;
     justify-content: space-around;
     align-items: center;
 
-    img{
-        width: 80px;
+    img {
+        width: 70px;
     }
 
-    ul{
+    ul {
         display: flex;
 
-        li{
+        li {
             padding: 0 10px;
+            font-size: $fontsize;
+
+            a {
+                padding: 5px;
+
+                &.active,
+                &:hover {
+                    color: $bluecolor;
+                    padding: 40px 5px;
+                    border-bottom: 2px solid $bluecolor;
+                }
+            }
         }
     }
 
