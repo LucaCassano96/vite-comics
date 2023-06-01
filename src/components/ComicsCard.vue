a061809e5c7c2349fb0ff5773ca4c89a1dffdb1a
Luca Cassano <lucacassano96@gmail.com>
2023-06-01 16:56:00 +0200

    cards nel main

---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,44 @@
+<script>
+
+export default {
+    name: "ComicsCard",
+    props:{
+        details: Object
+    }
+}
+</script>
+
+<template>
+    
+   
+
+    <div class="card">
+        <img :src="details.thumb" alt="img-card">
+        <div class="description">{{ details.series }}</div>
+    </div>
+
+     
+</template>
+
+<style lang="scss" scoped>
+@use "../styles/variables.scss" as *;
+
+
+    .card{
+        width: 120px;
+        height: 120px;
+        margin: 20px;
+
+        img{
+            width: 120px;
+            height: 120px;
+            object-fit: cover;
+        }
+
+        .description{
+            font-size: $fontsize;
+        }
+        }
+
+
+</style>
